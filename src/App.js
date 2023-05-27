38d3c9d300fff404626ee617610b72989d3daa9e
Pukii <salima.pusch@gmx.de>
2023-05-27 14:53:41 +0200

    added weather fetch functionality + TODO store geocoding autocomplete url in a literal and link useEffect to geocodingUrl update not value

--- src/App.js
+++ src/App.js
@@ -9,17 +9,56 @@ function App() {
   const delayTime = 3000;
   //Autocomplete API Key
   const autoAPIKey = "85d1e69a6f9b4e08b4226c0465384f18";
+  //weather API key
+  const weatherAPIKey = "020fdb36b8b179cde66157d24221cac6";
   //Autocomplete fetch URL
   const autocompleteUrl1 = "https://api.geoapify.com/v1/geocode/autocomplete?text="
   const autoCompleteUrl2 = "&type=city&format=json&apiKey=" + autoAPIKey;
-  
+  //weather url
+  const [weatherUrl, setWeatherUrl] = useState("");
   //search value
   const [value, setValue] = useState("");
   const [error, setError] = useState(null);
   const [loading, setLoading] = useState(false);
+  //weather data + weather fetch error
+  const [weatherError, setWeatherError] = useState(null);
+  const [weatherData, setWeatherData] = useState([]);
   //autocomplete suggestions
   const [suggestions, setSuggestions] = useState([]);
   
+
+  //callback to poplate weather data + weather error consts
+  const fetchWeatherCallback = (data, error)=>{
+    setWeatherData(data);
+    setWeatherError(error)
+  }
+  //trigger weather data fetch once coords change
+  useEffect(()=>{
+    console.log(`fetching ${weatherUrl}`)
+    fetch(weatherUrl)
+    .then(res => {
+      // If data fetching failed, throw an error with an error message
+      if (!res.ok) {
+        throw new Error("Error occurred while trying to fetch the weather data");
+      }
+      // If fetching succeeded, return the JSON of the response
+      return res.json();
+    })
+    .then(data => {
+      console.log("fetching weather data")
+      console.log(data)
+      fetchWeatherCallback(data.list[0].main, null);
+    })
+    .catch(err => {
+      fetchWeatherCallback( null, err.message);
+    });
+  }, [weatherUrl])
+
+  //setting new location coords
+  const setCoords = (lat, lon)=>{
+    setWeatherUrl(`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${weatherAPIKey}`);
+    console.log(`setting coords lat:${lat}, lon:${lon}`)
+  }
 
   //fetch callback
   const fetchCallback = (data, error, loading)=> {
@@ -61,9 +100,7 @@ function App() {
   };
   
   useEffect(() => {
-    if(true){
-    console.log("fetching...")
-
+    if(value.length > 0){
       const debouncedFetch = debounce(() => {
         setLoading(true);
         let autocompleteUrl = autocompleteUrl1 + value + autoCompleteUrl2;
@@ -81,7 +118,10 @@ function App() {
 
 
 
-  
+  //converts kelvin to celsius
+  const toCelsius = (k)=>{
+    return Math.floor(k-273.15)
+  }
   return (
     <div className="App">
       <h1>Weather App</h1>
@@ -95,8 +135,9 @@ function App() {
        {loading ? 
         <p>Loading...</p> : 
         <div>
-          {suggestions && <Dropdown suggestions={suggestions}/>}  
+          {suggestions && <Dropdown setCoords={setCoords} suggestions={suggestions}/>}  
         </div>}
+        {weatherData && <p>temp max: {toCelsius(weatherData.temp)}°</p>}
     </div>
   );
 }
--- src/components/Dropdown.jsx
+++ src/components/Dropdown.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import "../styles/Dropdown.scss"
 
-export default function Dropdown({suggestions}) {
+export default function Dropdown({suggestions, setCoords}) {
+  
   return (
     <div className="dropdown">
         <div className="dropdown-content">
 
             {suggestions.map((suggestion, i)=>{
-                return (<a href="#" key={i}>
+                return (<a href="#" onClick={()=>{setCoords(suggestion.lat, suggestion.lon)}} key={i}>
                     {suggestion.city},
                     {suggestion.country},
                     {suggestion.country_code}
